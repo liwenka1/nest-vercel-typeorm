26fe8ab29d2dc35916f2fe1bef0444ef3fde98c9
李文凯 <2020583117@qq.com>
2024-06-06 15:09:46 +0800

    fix: 问题找到了 是数据库连接的问题 后续想办法解决

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -6,11 +6,11 @@ export const databaseProviders = [
 		useFactory: async () => {
 			const dataSource = new DataSource({
 				type: 'postgres',
-				host: 'aws-0-ap-southeast-1.pooler.supabase.com',
-				port: 5432,
-				username: 'postgres.vaeesrjlsrdinuqeylfy',
-				password: 'QL1XcyZV4XIn645k',
-				database: 'postgres',
+				host: process.env.DB_HOST,
+				port: parseInt(process.env.DB_PORT, 10),
+				username: process.env.DB_USER,
+				password: process.env.DB_PASSWORD,
+				database: process.env.DB_NAME,
 				entities: [__dirname + '/../**/*.entity{.ts,.js}'],
 				synchronize: true,
 			});
